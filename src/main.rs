8de3b4cbbf3f394d90ab1e6153abfe1e5f44a2cc
mitsutoshi <mitsu3204@gmail.com>
2022-03-13 14:19:38 +0900

    Add first version

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    let mut lines: Vec<String> = Vec::new();
+    while io::stdin().read_line(&mut input).unwrap() > 0 {
+        let s = input.to_string();
+        if !lines.contains(&s) {
+            print!("{}", s);
+            lines.push(s);
+        }
+        input.clear();
+    }
+    //print!("{}", lines.join(""));
+}
